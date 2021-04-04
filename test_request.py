5c5eaffcd4cfc1b592cac6417c7701e12a741621
hlanetest <harrisonrlane3@gmail.com>
2021-04-04 03:41:13 -0400

    Split get_game_id & get_game_time into seperate functions for ease of data view

--- test_request.py
+++ test_request.py
@@ -10,6 +10,7 @@ game_id = handle.get_game_id()
 
 
 def json_builder(game_id):
+    '''The main driver that checks all the steam apps based upon appID and stores them into a Json array, along with the release date, recent, all reviews, and titles'''
     for i in game_id:
         try:
             url = "https://store.steampowered.com/app/{}".format(i)
@@ -41,28 +42,3 @@ def json_builder(game_id):
 
 
 json_builder(game_id)
-
-# appid = appIdList()
-# here we handle the request to the specified site, and then begin to parse the data
-# url ="https://store.steampowered.com/app/245370"
-# html_content = requests.get(url).text
-# soup = BeautifulSoup(html_content, 'lxml')
-
-# title = soup.title.text
-# recent_score = soup.find_all('in the last 30 days are positive')
-# dev = soup.find_all(id="developers_list")
-# describ_test = descrip.get_text()
-# class_='user_reviews'
-# all_score = soup.find_all()
-
-# html = list(soup.children)
-# body = list(html.children)[3]
-# test = list(body.children)
-# title = list(html.children)[1]
-# for link in soup.find_all("a"):
-#     print("Inner Text: {}".format(link.text))
-#     print("Title: {}".format(link.get("title")))
-#     print("class: {}".format(link.get("class")))
-# print(title)
-
-# /html/body/div[1]/div[7]/div[4]/div[1]/div[3]/div[4]/div[1]/div/div[1]/div/div[3]/div/div[1]/div[2]
