8393bec95c3f11a481d71d976a86630415654a6f
hlanetest <harrisonrlane3@gmail.com>
2021-04-05 00:27:15 -0400

    Added a new class to test_request and rearranged the order of some keys, name is now the second value rather than last

--- test_request.py
+++ test_request.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import steam_profile_cons
+import json
 
 
 steamid = input("Enter steam ID:")
@@ -9,36 +10,39 @@ handle = steam_profile_cons.handler(api_key, steamid)
 game_id = handle.get_game_id()
 
 
-def json_builder(game_id):
-    '''The main driver that checks all the steam apps based upon appID and stores them into a Json array, along with the release date, recent, all reviews, and titles'''
-    for i in game_id:
-        try:
-            url = "https://store.steampowered.com/app/{}".format(i)
-            html_content = requests.get(url).text
-            soup = BeautifulSoup(html_content, 'lxml')
-            title = soup.title.text
+class var_builder():
+    def __init__(self, game_id):
+        self.game_id = game_id
 
-            if title == "Welcome to Steam":
-                pass
-
+    def json_builder(self):
+        '''The main driver that checks all the steam apps based upon appID and stores them into a Json array, along with the release date, recent, all reviews, and titles'''
+        for i in game_id:
             try:
-                recent_review = soup.find(
-                    'span', class_="game_review_summary positive").text
+                url = "https://store.steampowered.com/app/{}".format(i)
+                html_content = requests.get(url).text
+                soup = BeautifulSoup(html_content, 'lxml')
+                title = soup.title.text
+
+                if title == "Welcome to Steam":
+                    pass
+
+                try:
+                    recent_review = soup.find(
+                        'span', class_="game_review_summary positive").text
+                except AttributeError:
+                    print(i, "Game {} has no recent reviews".format(
+                        title.replace('on Steam', '')))
+                all_review = soup.find(
+                    'span', attrs={'itemprop': 'description'}).text
+                release_date = soup.find(
+                    'div', class_='release_date').text.replace("\nRelease Date:\n", "").replace("\n", "")
+                game_id[i]['name'] = title.replace('on Steam', '')
+                game_id[i]['release_date'] = release_date
+                game_id[i]['recent_score'] = recent_review
+                game_id[i]['total_score'] = all_review
             except AttributeError:
-                print(i, "Game {} has no recent reviews".format(
-                    title.replace('on Steam', '')))
-            all_review = soup.find(
-                'span', attrs={'itemprop': 'description'}).text
-            release_date = soup.find(
-                'div', class_='release_date').text.replace("\nRelease Date:\n", "").replace("\n", "")
-            game_id[i]['release_date'] = release_date
-            game_id[i]['recent_score'] = recent_review
-            game_id[i]['total_score'] = all_review
-            game_id[i]['name'] = title.replace('on Steam', '')
-
-        except AttributeError:
-            print("unable to find store page. ", i)
-    print(game_id)
+                print("unable to find store page. ", i)
+        print(json.dumps(game_id, indent=2))
 
 
-json_builder(game_id)
+var_builder.json_builder(game_id)
